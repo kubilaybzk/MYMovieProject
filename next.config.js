714369a01bf2c49f70acafe37e9954fa629ffcb2
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-10-16 17:25:12 +0300

    .github/workflows silindi.
    .github/workflows silindi.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-   basePath: "/turktelekom_movie_project",
-  assetPrefix: "/turktelekom_movie_project",
 }
 
 module.exports = nextConfig
