5acd9b231e0d0c8225f07d4ae466d9afe4dafdb1
Kubilay Bozak <kubilaybozak@gmail.com>
2022-10-16 17:21:29 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+   basePath: "/turktelekom_movie_project",
+  assetPrefix: "/turktelekom_movie_project",
 }
 
 module.exports = nextConfig
